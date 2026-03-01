6d0bff8d5e5fde3284b13fb50d605273b97ebced
AADHITHYA-K <163022011+AADHITHYA-K@users.noreply.github.com>
2026-03-01 22:35:18 +0530

    databse
    Refactor Settings class to load environment variables from .env file.

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:Aadhi%401234@localhost:5432/hospital_db"
-    SECRET_KEY: str = "supersecretkey"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    DATABASE_URL: str
+    SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
